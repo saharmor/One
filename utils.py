a6ea25921e65bf574b708a16afe7af7d700f6bc0
Sahar Mor <saharhashai@gmail.com>
2023-11-03 17:03:28 -0700

    Fixed paper id extract func

--- utils.py
+++ utils.py
@@ -1,3 +1,16 @@
+def get_numeric_prefix(s):
+   result = []
+   for char in s:
+      if char.isdigit() or char == '.':
+         result.append(char)
+      else:
+         break  # Stop iterating at the first non-numeric, non-dot character
+
+   if result[-1] == '.':
+      result.pop()
+
+   return ''.join(result)
+
 def extract_paper_id_from_url(paper_url: str) -> str:
    # Remove the query string (if any) from the URL
    paper_url = paper_url.split('?')[0]
@@ -6,4 +19,12 @@ def extract_paper_id_from_url(paper_url: str) -> str:
    # Get the last part of the URL, which is the ID
    arxiv_id = url_parts[-1] if url_parts else None
 
-   return arxiv_id
+   if not arxiv_id:
+      return None
+   
+   arxiv_id = get_numeric_prefix(arxiv_id)
+   if len(arxiv_id) == 0:
+      return None
+   
+   # remove the ".pdf" suffix if exists
+   return arxiv_id.rstrip('.pdf')
